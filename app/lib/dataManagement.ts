5379e49212f225436ff4268c6e1e66e7d7d25885
louWertman <lwertman@tuta.io>
2025-01-21 21:00:50 -0500

    added loading and list functions to synfToFile class, added documentation and Papaparse for file parsing to the project, also updated the readme

--- app/lib/dataManagement.ts
+++ app/lib/dataManagement.ts
@@ -3,6 +3,9 @@ Authors: Lou Wertman,
 Purpose: Functions related to syncing to the local file or the database and any entry retrieval functions
 */
 
+import papaparse from "papaparse";
+import * as fs from 'fs';
+
 class syncToDB {
     public constructor(public syncToken: string){
         this.syncToken = syncToken;
@@ -11,18 +14,50 @@ class syncToDB {
 }
 
 class syncToFile {
-    public constructor(public file: string){
-        this.file = file;
+    //constructor
+    public constructor(public filePath: string){
+        this.filePath = filePath;
     }
 
+    //variables
+    private entryLog = this.loadFile(this.filePath);
+
+    //functions
+
+    //load and parse a file for the entryLog object
+    loadFile(filePath: string){
+        //if file does not exist create it
+        if(!fs.existsSync(filePath)){
+            fs.writeFileSync(filePath, 'DATE@~~@DELIM@~~@MOOD @~~@DELIM@~~@HABIT@~~@DELIM@~~@ENTRY\n')
+        }
+        //open and parse file
+        let file = fs.readFileSync(filePath, 'utf-8');
+        let data = papaparse.parse(file, {
+            delimiter: "@~~@DELIM@~~@",
+            skipEmptyLines: true,
+            header: true,
+            error: (error) => {
+                document.write('Error parsing file: ', error);
+            }
+        });
+        // data with multiple values such as mood and habits need to manually parsed later as they are seperate by comma values, for the time being they are stored as strings
+        let entryLog = data.data as {
+            date: string, mood: string, habits: string, entry: string
+        }[];
+        return entryLog;
+    }
+
+    //returns a string array with the date of every entry
     listEntries(){
-        ;
+        let dates = this.entryLog.map((row) => row.date);
+        return dates;
     }
-
+    //finds an entry with a date, returns an Entry object
     fetchEntry(date: string){
         ;
     }
 
+    //writes an entyr to the file, NULL return
     writeEntryToFile(entry:Entry){
         ;
     }
