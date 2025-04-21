0ffe34f749949bcf550209e7953ad07f595a043e
louWertman <lwertman@tuta.io>
2025-04-21 08:39:22 -0400

    Android set up
    set up android support

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,13 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.lifeLog',
   appName: 'lifeLog',
-  webDir: 'build',
+  webDir: '.next',
+  bundledWebRuntime: false,
+  server:{
+    androidScheme: 'https',
+    cleartext: true,
+  }
+  android: {}
 };
 
 export default config;
