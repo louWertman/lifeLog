f93d8000cacabe4faefc961754d2625342271e27
louWertman <lwertman@tuta.io>
2025-04-21 21:59:08 -0400

    Chart added

--- components/StatisticsV.tsx
+++ components/StatisticsV.tsx
@@ -7,77 +7,104 @@ import { Chart } from './chart';
 
 const Statistics: React.FC = () => {
     console.log("STATISTICS")
-  let fileSystem = new FileSystem();
-
-  //GUI is for display, but raw data is passed to chart component
-  const [allHabitsGUI, setAllHabitsGUI] = useState<string[]>([]);
-  const [allMoodsGUI, setAllMoodsGUI] = useState<string[]>([]);
-
-  const [selectedHabit, setSelectedHabit] = useState<string | null>(null);
-  const [selectedMood, setSelectedMood] = useState<string | null>(null);
-  const [allHabits, setAllHabits] = useState<string[]>([]);
-  const [allMoods, setAllMoods] = useState<string[]>([]);
-
-  useEffect(() => {
     const fs = new FileSystem();
-    const fetchHabits = async () => {
-        let habitsList = await fs.habitArrStatistics(); 
-        const habitNames = habitsList.map((habit) => habit.name);
-        setAllHabits(habitNames);
-        const uniqueHabits = Array.from(new Set(habitNames)); // Deduplicate
-        setAllHabitsGUI(uniqueHabits); 
 
-    };
-    const fetchMoods = async () => {
-        const moodsList: string[] = await fs.listAllMoods();
-        const moodNames = moodsList.map((mood) => mood); 
-        console.log("MOODS: ", moodNames);
-        setAllMoods(moodsList);
-        const uniqueMoods = Array.from(new Set(moodNames)); // Deduplicate
-        setAllMoodsGUI(uniqueMoods);
-    };
+    //GUI is for display, but raw data is passed to chart component
+    const [allHabitsGUI, setAllHabitsGUI] = useState<string[]>([]);
+    const [allMoodsGUI, setAllMoodsGUI] = useState<string[]>([]);
 
-    fetchHabits();
-    fetchMoods();
-  }, []);
-  return (
-    <div>
+    const [selectedHabit, setSelectedHabit] = useState<string | null>(null);
+    const [allHabits, setAllHabits] = useState<string[]>([]);
+    const [allMoods, setAllMoods] = useState<string[]>([]);
+
+    const [chartData, setChartData] = useState<any[]>([]);
+
+    useEffect(() => {
+        const fetchHabits = async () => {
+            let habitsList = await fs.listAllHabits();
+            const habitNames = habitsList.map((habit) => habit.name);
+            setAllHabits(habitNames);
+            setAllHabitsGUI(habitNames);
+
+        };
+        const fetchMoods = async () => {
+            const moodsList: string[] = await fs.listAllMoods();
+            const moodNames = moodsList.map((mood) => mood);
+            console.log("MOODS: ", moodNames);
+            setAllMoods(moodsList);
+            const uniqueMoods = Array.from(new Set(moodNames)); // Deduplicate
+            setAllMoodsGUI(uniqueMoods);
+        };
+
+
+        fetchHabits();
+        fetchMoods();
+    }, []);
+    useEffect(() => {
+        const dataProc = async () => {
+            if (!selectedHabit) return;
+
+            const entryLog = await fs.entryLog;
+            const moodCounts: { [mood: string]: number } = {};
+
+            for (const entry of entryLog) {
+                const habits = entry.habits.map((habit: { name: string }) => habit.name);
+                if (habits.includes(selectedHabit)) {
+                    const mood = entry.mood;
+                    if (!moodCounts[mood]) {
+                        moodCounts[mood] = 0;
+                    }
+                    moodCounts[mood]++;
+                }
+            }
+
+            if (Object.keys(moodCounts).length === 0) {
+                const allMoods = await fs.listAllMoods();
+                allMoods.forEach((mood) => {
+                    if (!moodCounts[mood]) {
+                        moodCounts[mood] = 0;
+                    }
+                });
+            }
+
+            const data = Object.entries(moodCounts).map(([mood, count]) => ({
+                mood,
+                count,
+            }));
+
+            console.log("DATA from STATVIEW: ", data);
+            console.log("STATVIEW: for habit: " + selectedHabit);
+
+            setChartData(data);
+        };
+        dataProc();
+    }, [selectedHabit]);
+    return (
         <div>
-            <label htmlFor="habit-select">Select Habit:</label>
-            <select id="habit-select"
-                onChange={(e) => {
-                    const habit = e.target.value;
-                    setSelectedHabit(habit);
-                }}>
-                {allHabitsGUI.map((habit, index) => (
-                    <option key={index} value={habit}>
-                        {habit}
+            <div>
+                <label htmlFor="habit-select">Select Habit:</label>
+                <select id="habit-select"
+                    onChange={(e) => {
+                        const habit = e.target.value;
+                        setSelectedHabit(habit);
+                    }}>
+                    <option value="" disabled selected>
+                        -- Select a Habit --
                     </option>
-                ))}
-            </select>
-            <label htmlFor="mood-select">Select Mood:</label>
-            <select id="mood-select"
-                onChange={(e) => {
-                    const mood = e.target.value;
-                    setSelectedMood(mood);
-                }}>
-                {allMoodsGUI.map((mood, index) => (
-                    <option key={index} value={mood}>
-                        {mood}
-                    </option>
-                ))}
-            </select>
+                    {allHabitsGUI.map((habit, index) => (
+                        <option key={index} value={habit}>
+                            {habit}
+                        </option>
+                    ))}
+                </select>
+            </div>
+            <div>
+                <Chart
+                    data={chartData}
+                />
+            </div>
         </div>
-        <div>
-        <Chart
-            allHabits={allHabits}
-            allMoods={allMoods}
-            selectedHabit={selectedHabit}
-            selectedMood={selectedMood}
-        />
-        </div>
-    </div>
-  );
+    );
 };
 
 export default Statistics
--- components/chart.tsx
+++ components/chart.tsx
@@ -1,78 +1,39 @@
-import React, { useEffect, useState, PureComponent } from 'react';
+import React, { useEffect, useState, PureComponent, useMemo } from 'react';
 import { FileSystem } from '../app/lib/dataManagement';
 import { Habit, Entry } from '../app/lib/entity';
 import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend } from 'recharts';
 
 interface chartProps {
-    allHabits: string[];
-    allMoods: string[];
-    selectedHabit: string | null;
-    selectedMood: string | null;
+    data: any[];
 }
 
-export const Chart: React.FC<chartProps> = ({ allHabits, allMoods, selectedHabit, selectedMood }) => {
-    const [habitList, setHabitList] = useState<string[]>([]);
-    const [moodList, setMoodList] = useState<string[]>([]);
-    const [habitSelect, setHabitSelect] = useState<string | null>(null);
-    const [moodSelect, setMoodSelect] = useState<string | null>(null);
-
+export const Chart: React.FC<chartProps> = ({ data }) => {
     const [chartData, setChartData] = useState<any[]>([]);
-
     useEffect(() => {
-        setHabitList(allHabits);
-        setMoodList(allMoods);
-        setHabitSelect(selectedHabit);
-        setMoodSelect(selectedMood);
+        if (data && data.length > 0) {
+            setChartData(data);
+        }
+    }, [data]);
 
-        const dataProc = async () => {
-            const data: any[] = [];
-            const fs = new FileSystem();
-            const el = fs.entryLog;
-            let habitMoodData: {
-                [habit: string]: {
-                    [mood: string]: number;
-                }
-            } = {};
-            for (let entry of await el) {
-                const habits = (entry.habits as Habit[]).map(habit => habit.name);
-                for (const habit of habits) {
-                    const mood = entry.mood;
-                    if (!habitMoodData[habit]) {
-                        habitMoodData[habit] = {};
-                    }
-                    if (!habitMoodData[habit][mood]) {
-                        habitMoodData[habit][mood] = 0;
-                    }
-                    habitMoodData[habit][mood]++;
-                }
-            }
-                setChartData(Object.entries(habitMoodData).map(([habit, moods]) => ({
-                    habit,
-                    ...moods,
-                })));
-            };
-            dataProc();
-        }, [allHabits, allMoods, selectedHabit, selectedMood]);
 
-    if (habitList.length === 0 || moodList.length === 0) {
-        return <div>There are no entries to load data from</div>;
+    if (!data || data.length === 0) {
+        return <div>Not enough data! Use lifeLog more or maybe there is not enough information on this particular habit</div>;
     }
 
     return (
         <BarChart
-        width={800}
-        height={400}
-        data={chartData}
-        margin={{ top: 20, right: 30, left: 20, bottom: 5 }}
-    >
-        <CartesianGrid strokeDasharray="3 3" />
-        <XAxis dataKey="habit" />
-        <YAxis />
-        <Tooltip />
-        <Legend />
-        {allMoods.map((mood) => (
-            <Bar key={mood} dataKey={mood} fill={`#${Math.floor(Math.random() * 16777215).toString(16)}`} />
-        ))}
-    </BarChart>
+            width={800}
+            height={400}
+            data={data}
+            margin={{ top: 20, right: 30, left: 20, bottom: 5 }}>
+
+            <CartesianGrid strokeDasharray="3 3" />
+            <XAxis dataKey="mood" />
+            <YAxis />
+            <Tooltip />
+            <Legend />
+
+            <Bar dataKey="count" fill="#8884d8" />
+        </BarChart>
     );
 }
